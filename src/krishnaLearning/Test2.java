bfd1eac9367a335c73a7263db42c18eddb3315a7
Krishna2005 <knavneetkumar768@gmail.com>
2025-09-22 19:01:10 +0530

    static
    In this I have discuused about static function

--- src/krishnaLearning/Test2.java
+++ src/krishnaLearning/Test2.java
@@ -1,5 +1,26 @@
 package krishnaLearning;
 
-public class Test2 {
-
+ class Area{
+	static void fun1(){
+		System.out.println("E");
+	}
+	static void fun2() {
+		System.out.println("G");
+	}
 }
+	 class Test2 {
+		static void fun1() {
+			System.out.println("A");
+		}
+		static void fun2() {
+			System.out.println("B");
+		}
+	
+	public static void main (String args[]) {
+		System.out.println("C");
+		Area.fun1();
+		Test2.fun1();
+	}
+	}
+
+
