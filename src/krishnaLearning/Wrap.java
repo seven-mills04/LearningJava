ca7197bd98348780bac9af853a00cbbdfb302f88
Krishna2005 <knavneetkumar768@gmail.com>
2025-09-30 18:30:33 +0530

    Wrapper
    In this we are using wrapper class with class like Integer and Double

---
+++ src/krishnaLearning/Wrap.java
@@ -0,0 +1,14 @@
+package krishnaLearning;
+
+public class Wrap {
+public static void main(String []args) {
+	int x=Integer.parseInt("123");
+	System.out.println(x);
+	Integer n1=Integer.valueOf("10101110",2);
+	int y=n1.intValue();
+	System.out.println(y);
+	Double d1=Double.valueOf("4532");
+	double z=d1.doubleValue();
+System.out.println(z);
+}
+}
